79c951b275ce8dd9cae590c64d49fe87648b8864
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-15 12:47:32 +0300

    Register tables from model

--- DjangoAPP/admin.py
+++ DjangoAPP/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from DjangoAPP.models import ExchangeProviders, ExchangeRates
+
+admin.site.register([ExchangeProviders, ExchangeRates])
