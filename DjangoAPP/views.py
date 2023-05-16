e2ff113efe84c950ccc7e6ef8dcb839719a828c5
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-16 12:38:04 +0300

    default index views

--- DjangoAPP/views.py
+++ DjangoAPP/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from DjangoAPP.services import ExchangePrivate24Service
+
+
+def index(request):
+    privatbank = ExchangePrivate24Service()
+    data = privatbank.get_data()
+    print(data)
+    return render(request, 'index.html')
