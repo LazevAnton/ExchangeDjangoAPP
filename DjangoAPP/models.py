85760e44b89b9a8a56125999a1db7f02acb7a830
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-16 12:37:22 +0300

    add date_rate to ExchangeRates

--- DjangoAPP/models.py
+++ DjangoAPP/models.py
@@ -5,12 +5,16 @@ class ExchangeProviders(models.Model):
     provider_name = models.CharField(max_length=20, blank=False)
     provider_api_url = models.URLField(blank=False)
 
+    def __str__(self):
+        return f'{self.provider_name}'
+
 
 class ExchangeRates(models.Model):
     base_currency = models.CharField(max_length=20)
     currency = models.CharField(max_length=20)
     sale_rate = models.DecimalField(max_digits=10, decimal_places=4)
     buy_rate = models.DecimalField(max_digits=10, decimal_places=4)
+    date_rate = models.CharField(blank=True, max_length=20)
     provider = models.ForeignKey(ExchangeProviders, on_delete=models.CASCADE)
 
     def __str__(self):
